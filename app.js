496c2c36f64c67ca4260c499d7e1f20028a34183
Adam Shen <adamshen1997@gmail.com>
2017-10-08 01:32:29 -0700

    app is updated
    app is updated

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require('express')
 const app = express()
-
+var multer = require('multer')
+var upload = multer({ dest: 'uploads/' })
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT, function () {
   console.log('Example app listening on port 3000!')
+})
+
+const fs = require ('fs')
+
+app.post('/image', upload.single('file'), function (req, res, next) {
+  console.log('got image', req.file)
+  // Instantiates a client
+  const vision = Vision();
+  // The path to the local image file, e.g. "/path/to/image.png"
+   var fileName = req.file.path+'.jpg';
+   var landmarks = {}
+  // Performs landmark detection on the local file
+  vision.landmarkDetection({ source: {filename: fileName} })
+    .then((results) => {
+      landmarks = results[0].landmarkAnnotations;
+      console.log('Landmarks:');
+      landmarks.forEach((landmark) => console.log(landmark));
+    })
+  .catch((err) => {
+    console.error('ERROR:', err);
+  });
+  res.send(landmarks)
 })
